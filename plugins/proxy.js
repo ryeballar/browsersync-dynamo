e7a254c573dbedee2009973a1e4c8836a1f0eb21
Ryan Eballar <ryeballar@gmail.com>
2016-05-13 15:28:29 +0800

    added dynamic proxy options

--- plugins/proxy.js
+++ plugins/proxy.js
@@ -1,17 +1,96 @@
-var bsUtil = require('browser-sync/lib/server/utils');
+var fs = require('fs');
+var _ = require('lodash');
+
+var compiled = _.template(fs.readFileSync(__dirname + '/proxy.html'));
+var id = 'bs-' + Math.random().toString(36).substr(2);
 
 module.exports = function(bs) {
 
-	var bsUtilGetServer = bsUtil.getServer;
-	bsUtil.getServer = function(app, options) {
+	var bsCreate = bs.create;
 
-		var proxy = options.get('proxy');
-		if(proxy && proxy.get('target')) {
-			options.set('https', proxy.get('target').indexOf('https') === 0);
-		}
+	bs.create = function() {
 
-		return bsUtilGetServer.call(bsUtil, app, options);
+		var instance = bsCreate.apply(bs, arguments);
+		var instanceInit = instance.init;
+
+		var reload = function() {
+			setTimeout(function() {
+				instance.reload();
+			}, 2000);
+		};
+
+		instance.init = function(config) {
+
+			if(config.dynamicproxy) {
+
+				if(['undefined', 'string'].indexOf(typeof config.proxy) >= 0) {
+					config.proxy = {
+						target: config.dynamicproxy.targets[0]
+					};
+				}
+
+				config.proxy.middleware = function(req, res, next) {
+
+					var url;
+
+					if(req.url.indexOf(id) >= 0) {
+
+						url = decodeURIComponent(req.url.split('=').pop());
+						config.proxy.target = url;
+						instance.exit();
+						instance.init(config);
+
+					} else {
+
+						next();
+
+					}
+
+
+				};
+
+				config.snippetOptions = {
+
+					rule: {
+						match: /<\/body>/i,
+						fn: function (snippet, match) {
+							var proxies = config.dynamicproxy.targets.filter(function(target) {
+								return config.proxy.target.indexOf(target) < 0;
+							});
+							return snippet +
+								compiled({
+									proxies: proxies,
+									id: id
+								}) +
+								match;
+						}
+					}
+
+				};
+
+			}
+
+			if(config.proxy) {
+				config.https = isHttps(
+					typeof config.proxy === 'string'?
+						config.proxy: config.proxy.target
+				);
+			}
+
+			reload();
+
+			return instanceInit.apply(instance, arguments);
+
+		};
+
+		return instance;
+
 
 	};
 
+
 };
+
+function isHttps(target) {
+	return target.indexOf('https') === 0;
+}
